99e6aa7d1ad2f549b186c1f9d2880941e599aa4b
Conectar Wali SAS <167815566+ConectarWali@users.noreply.github.com>
2025-01-16 13:32:32 -0500

    update integration __init__ package

--- integration/__init__.py
+++ integration/__init__.py
@@ -1,5 +1,6 @@
 from integration.connect_to import connect_to
 from integration.integration import Integration
+from exception.integration_error import Integration_error
 
 __version__ = '0.0.1'
-__all__ = ['Integration', 'connect_to']
+__all__ = ['Integration', 'connect_to', 'Integration_error']
